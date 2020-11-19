f99dcebb1bdb23ceae6e17d757a3da5307b91a38
Nifemi <deyeminifemi@gmail.com>
2020-11-19 21:23:09 +0100

    Setup add to cart

--- script.js
+++ script.js
@@ -1,189 +1,179 @@
 const domElements = {
-    container: document.querySelector('.container'),
-    cartIcon: document.querySelector('.cart-icon'),
-    addCart: document.querySelectorAll('.add-cart'),
-    itemCount: document.querySelector('.item-count'),
-    cart: document.querySelector('.cart'),
-    hatOne: document.getElementById('hat-one')
+  container: document.querySelector('.container'),
+  cartIcon: document.querySelector('.cart-icon'),
+  addCart: document.querySelectorAll('.add-cart'),
+  itemCount: document.querySelector('.item-count'),
+  cart: document.querySelector('.cart'),
+  hatOne: document.getElementById('hat-one')
 };
 
-let counter = 0;
-let counter1 = 0;
-let counter2 = 0;
-let counter3 = 0;
-let counter4 = 0;
-let counter5 = 0;
-let counter6 = 0;
-let counter7 = 0;
-let counter8 = 0;
-let counter9 = 0;
-let counter10 = 0;
-let counter11 = 0;
-let counter12 = 0;
+let cart = [];
+let itemsInCart = 0;
 
 const categories = [
-    {
-        id: 'hats',
-        name: 'Hats',
-        items: [
-            {
-                name: 'Brown Brim',
-                image: 'brown-brim.png',
-                price: 25
-            },
-            {
-                name: 'Brown Brim',
-                image: 'brown-brim.png',
-                price: 25
-            },
-            {
-                name: 'Brown Brim',
-                image: 'brown-brim.png',
-                price: 25
-            },
-            {
-                name: 'Brown Brim',
-                image: 'brown-brim.png',
-                price: 25
-            },
-        ]
-    },
-    {
-        id: 'sneakers',
-        name: 'Sneakers',
-        items: [
-            {
-                name: 'Brown Brim',
-                image: 'brown-brim.png',
-                price: 25
-            },
-            {
-                name: 'Brown Brim',
-                image: 'brown-brim.png',
-                price: 25
-            },
-            {
-                name: 'Brown Brim',
-                image: 'brown-brim.png',
-                price: 25
-            },
-            {
-                name: 'Brown Brim',
-                image: 'brown-brim.png',
-                price: 25
-            },
-        ]
-    },
-]
+  {
+    id: 'hats',
+    name: 'Hats',
+    items: [
+      {
+        name: 'Brown Brim',
+        image: 'brown-brim.png',
+        price: 25
+      },
+      {
+        name: 'Brown Brim',
+        image: 'brown-brim.png',
+        price: 25
+      },
+      {
+        name: 'Brown Brim',
+        image: 'brown-brim.png',
+        price: 25
+      },
+      {
+        name: 'Brown Brim',
+        image: 'brown-brim.png',
+        price: 25
+      },
+    ]
+  },
+  {
+    id: 'sneakers',
+    name: 'Sneakers',
+    items: [
+      {
+        name: 'Brown Brim',
+        image: 'brown-brim.png',
+        price: 25
+      },
+      {
+        name: 'Brown Brim',
+        image: 'brown-brim.png',
+        price: 25
+      },
+      {
+        name: 'Brown Brim',
+        image: 'brown-brim.png',
+        price: 25
+      },
+      {
+        name: 'Brown Brim',
+        image: 'brown-brim.png',
+        price: 25
+      },
+    ]
+  },
+];
 
 categories.forEach(category => {
-    const categoryDiv = document.createElement('div')
-    categoryDiv.classList.add('items-container')
+  const categoryDiv = document.createElement('div');
+  categoryDiv.classList.add('items-container');
 
-    const categoryHeader = document.createElement('h2')
-    categoryHeader.innerText = category.name
+  const categoryHeader = document.createElement('h2');
+  categoryHeader.innerText = category.name;
 
-    const itemsWrapper = document.createElement('div')
-    itemsWrapper.classList.add('items')
+  const itemsWrapper = document.createElement('div');
+  itemsWrapper.classList.add('items');
 
-    category.items.forEach(item => {
-        const itemDiv = document.createElement('div')
+  category.items.forEach((item, idx) => {
+    const itemDiv = document.createElement('div');
 
-        const imageDiv = document.createElement('div')
-        imageDiv.classList.add('item')
-        imageDiv.style.backgroundImage = `url('/assets/${item.image}')`
+    const imageDiv = document.createElement('div');
+    imageDiv.classList.add('item');
+    imageDiv.style.backgroundImage = `url('/assets/${item.image}')`;
 
-        const addButton = document.createElement('button')
-        addButton.classList.add('add-cart')
-        addButton.innerText = 'Add to Cart'
+    const addButton = document.createElement('button');
+    addButton.classList.add('add-cart');
+    addButton.innerText = 'Add to Cart';
 
-        imageDiv.append(addButton)
+    addButton.addEventListener('click', () => {
+      const itemId = `${category.id}-${idx}`;
 
+      const prevItem = cart.find(item => item.id === itemId);
 
-        const itemDetails = document.createElement('div')
-        itemDetails.classList.add('item-details')
+      if (prevItem) {
+        prevItem.quantity++;
+      } else {
+        cart.push({
+          ...item,
+          id: itemId,
+          quantity: 1
+        });
+      }
 
-        const itemTitle = document.createElement('span')
-        itemTitle.classList.add('title')
-        itemTitle.innerText = item.name
+      itemsInCart++
 
-        const itemPrice = document.createElement('span')
-        itemPrice.classList.add('price')
-        itemPrice.innerText = item.price
+      renderCartItems()
+    });
 
-        itemDetails.append(itemTitle)
-        itemDetails.append(itemPrice)
-
-        itemDiv.append(imageDiv)
-        itemDiv.append(itemDetails)
-
-        itemsWrapper.append(itemDiv)
-    })
-
-    categoryDiv.append(categoryHeader)
-    categoryDiv.append(itemsWrapper)
-
-    domElements.container.append(categoryDiv)
-})
+    imageDiv.append(addButton);
 
 
-domElements.cartIcon,addEventListener('click', (e) => {
-    if(e.target.closest('.cart-icon')) {
-        domElements.cart.classList.toggle('show');
-    }
-})
+    const itemDetails = document.createElement('div');
+    itemDetails.classList.add('item-details');
 
+    const itemTitle = document.createElement('span');
+    itemTitle.classList.add('title');
+    itemTitle.innerText = item.name;
 
-domElements.container.addEventListener('click', (e) => {
-    let itemName;
-    let itemPrice;
-    let markup;
+    const itemPrice = document.createElement('span');
+    itemPrice.classList.add('price');
+    itemPrice.innerText = item.price;
 
-    if(e.target.closest('.item')) {
-        counter += 1;
-    }
-    domElements.itemCount.textContent = counter;
-    if (e.target.id === 'hat-one') {
-        counter1 += 1;
-        itemName = e.target.parentElement.parentElement.lastElementChild.firstElementChild.textContent;
-        itemPrice = e.target.parentElement.parentElement.lastElementChild.lastElementChild.textContent;
+    itemDetails.append(itemTitle);
+    itemDetails.append(itemPrice);
 
-        markup = `
-        <div class = "cart-item">
-            <div class = "item-image">
-                <img src = "assets/${e.target.parentElement.id}.png" />
-            </div>
-            <div class = "cart-item-details">
-                <p>${itemName}</p>
-                <p>${counter1} X ${itemPrice}</p>
-            </div>
-        </div>`;
+    itemDiv.append(imageDiv);
+    itemDiv.append(itemDetails);
 
-        domElements.cart.insertAdjacentHTML('beforeend', markup);
+    itemsWrapper.append(itemDiv);
+  });
 
-    }
-    if (e.target.id === 'hat-two') {
-        counter2 += 1;
-        itemName = e.target.parentElement.parentElement.lastElementChild.firstElementChild.textContent;
-        itemPrice = e.target.parentElement.parentElement.lastElementChild.lastElementChild.textContent;
+  categoryDiv.append(categoryHeader);
+  categoryDiv.append(itemsWrapper);
 
-        markup = `
-        <div class = "cart-item">
-            <div class = "item-image">
-                <img src = "assets/${e.target.parentElement.id}.png" />
-            </div>
-            <div class = "cart-item-details">
-                <p>${itemName}</p>
-                <p>${counter2} X ${itemPrice}</p>
-            </div>
-        </div>`;
+  domElements.container.append(categoryDiv);
+});
 
-        domElements.cart.insertAdjacentHTML('beforeend', markup);
-    }
-})
+const renderCartItems = () => {
+  domElements.cart.innerHTML = '';
 
+  /*
+  * name
+  * id
+  * image
+  * quantity
+  * price
+  * */
 
+  cart.forEach(cartItem => {
+    const markUp = `
+      <div class="cart-item">
+        <div class="item-image">
+          <img 
+            src="assets/${cartItem.image}" 
+            alt=${cartItem.name} 
+          />
+        </div>
+        <div class="item-details">
+          <p>${cartItem.name}</p>
+          <p>${cartItem.quantity} X ${cartItem.price}</p>
+        </div>
+      </div>
+    `;
 
+    domElements.cart.innerHTML += markUp
+  });
+
+  domElements.itemCount.innerHTML = itemsInCart
+
+};
+
+domElements.cartIcon.addEventListener('click', (e) => {
+  if (e.target.closest('.cart-icon')) {
+    domElements.cart.classList.toggle('show');
+  }
+});
 
 
 // domElements.hatOne.addEventListener('click', () => {
