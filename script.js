b5bdd8aceba95bcebe3afd4811db3bb29588361e
Nifemi <deyeminifemi@gmail.com>
2020-11-19 21:03:28 +0100

    Generated categories from js

--- script.js
+++ script.js
@@ -21,6 +21,112 @@ let counter10 = 0;
 let counter11 = 0;
 let counter12 = 0;
 
+const categories = [
+    {
+        id: 'hats',
+        name: 'Hats',
+        items: [
+            {
+                name: 'Brown Brim',
+                image: 'brown-brim.png',
+                price: 25
+            },
+            {
+                name: 'Brown Brim',
+                image: 'brown-brim.png',
+                price: 25
+            },
+            {
+                name: 'Brown Brim',
+                image: 'brown-brim.png',
+                price: 25
+            },
+            {
+                name: 'Brown Brim',
+                image: 'brown-brim.png',
+                price: 25
+            },
+        ]
+    },
+    {
+        id: 'sneakers',
+        name: 'Sneakers',
+        items: [
+            {
+                name: 'Brown Brim',
+                image: 'brown-brim.png',
+                price: 25
+            },
+            {
+                name: 'Brown Brim',
+                image: 'brown-brim.png',
+                price: 25
+            },
+            {
+                name: 'Brown Brim',
+                image: 'brown-brim.png',
+                price: 25
+            },
+            {
+                name: 'Brown Brim',
+                image: 'brown-brim.png',
+                price: 25
+            },
+        ]
+    },
+]
+
+categories.forEach(category => {
+    const categoryDiv = document.createElement('div')
+    categoryDiv.classList.add('items-container')
+
+    const categoryHeader = document.createElement('h2')
+    categoryHeader.innerText = category.name
+
+    const itemsWrapper = document.createElement('div')
+    itemsWrapper.classList.add('items')
+
+    category.items.forEach(item => {
+        const itemDiv = document.createElement('div')
+
+        const imageDiv = document.createElement('div')
+        imageDiv.classList.add('item')
+        imageDiv.style.backgroundImage = `url('/assets/${item.image}')`
+
+        const addButton = document.createElement('button')
+        addButton.classList.add('add-cart')
+        addButton.innerText = 'Add to Cart'
+
+        imageDiv.append(addButton)
+
+
+        const itemDetails = document.createElement('div')
+        itemDetails.classList.add('item-details')
+
+        const itemTitle = document.createElement('span')
+        itemTitle.classList.add('title')
+        itemTitle.innerText = item.name
+
+        const itemPrice = document.createElement('span')
+        itemPrice.classList.add('price')
+        itemPrice.innerText = item.price
+
+        itemDetails.append(itemTitle)
+        itemDetails.append(itemPrice)
+
+        itemDiv.append(imageDiv)
+        itemDiv.append(itemDetails)
+
+        itemsWrapper.append(itemDiv)
+    })
+
+    categoryDiv.append(categoryHeader)
+    categoryDiv.append(itemsWrapper)
+
+    domElements.container.append(categoryDiv)
+})
+
+
 domElements.cartIcon,addEventListener('click', (e) => {
     if(e.target.closest('.cart-icon')) {
         domElements.cart.classList.toggle('show');
@@ -52,7 +158,7 @@ domElements.container.addEventListener('click', (e) => {
                 <p>${counter1} X ${itemPrice}</p>
             </div>
         </div>`;
-        
+
         domElements.cart.insertAdjacentHTML('beforeend', markup);
 
     }
@@ -71,7 +177,7 @@ domElements.container.addEventListener('click', (e) => {
                 <p>${counter2} X ${itemPrice}</p>
             </div>
         </div>`;
-        
+
         domElements.cart.insertAdjacentHTML('beforeend', markup);
     }
 })
