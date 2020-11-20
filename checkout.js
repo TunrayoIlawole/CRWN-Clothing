db2ef5250487806da1e4f3cae2f570e2f4e8597e
Nifemi <deyeminifemi@gmail.com>
2020-11-20 13:10:21 +0100

    Added TODO

--- checkout.js
+++ checkout.js
@@ -25,3 +25,14 @@ window.addEventListener('load', () => {
 
   window.renderCartItems(cart, itemsInCart);
 })
+
+/* TODO:
+   1. create renderCheckoutItems that'll render the cart items using the markup in your checkout.html
+   PS You have to actually create the elements cause you need to add event listeners to the increase, decrease and remove functions
+   2. create increaseQuantity function (parameters: itemId)
+   3. create reduceQuantity function (parameters: itemId)
+   4. create removeItem function (parameters: itemId)
+   5. Use these functions in your event listeners for the afore mentioned buttons in the P.S of number one
+   6. Send money to Nifemi cause he's amazing (or just cake I feel like taking cake rn idk why)
+
+ */
